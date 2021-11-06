7d10c34bd7649cce445e4ad8d31ec4336b5dca4a
kereplape <kiryaev.developer@gmail.com>
2021-11-06 12:13:43 +0500

    Added doc to createClient

--- src/domain/client.js
+++ src/domain/client.js
@@ -1,4 +1,11 @@
-
+/**
+ * Creates object containing information about the client
+ *
+ * @param number Required
+ * @param name Optional
+ * @param email Optional
+ * @return {{number: string, name: string|null, email: string|null}}
+ */
 function createClient(number, name, email) {
     if (!number) {
         throw new Error("A number must be provided")
