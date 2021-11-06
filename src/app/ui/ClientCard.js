89bf118be744a066be8c8a3099dba085d7ed6499
kereplape <kiryaev.developer@gmail.com>
2021-11-06 15:11:21 +0500

    Added ClientCard

---
+++ src/app/ui/ClientCard.js
@@ -0,0 +1,54 @@
+import {MenuRounded, MoreVertOutlined} from "@material-ui/icons"
+import React from "react"
+import {Card, IconButton, Menu, MenuItem} from "@material-ui/core"
+
+import "./ClientCard.css"
+
+function ClientCard(params) {
+    const {client, onEdit, onDelete} = params
+    const {number, name, email} = client
+
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+
+    function closeMenu() {
+        setAnchorEl(null)
+    }
+
+    function performEdit() {
+        onEdit(client)
+        closeMenu()
+    }
+
+    function performDelete() {
+        onDelete(client)
+        closeMenu()
+    }
+
+    return (
+        <Card className="client-card" component="section">
+            <header>
+                <h1 className="number">{number}</h1>
+                <IconButton className="options-button" onClick={(it) => setAnchorEl(it.currentTarget)}>
+                    <MoreVertOutlined />
+                </IconButton>
+                <Menu anchorEl={anchorEl} open={open} onClose={closeMenu}>
+                    <MenuItem onClick={performEdit}>Edit</MenuItem>
+                    <MenuItem onClick={performDelete}>Delete</MenuItem>
+                </Menu>
+            </header>
+            <div className="content">
+                <span className="detail">
+                    <h2 className="prefix">Name</h2>
+                    <p className="info">{name || "Unspecified"}</p>
+                </span>
+                <span className="detail">
+                    <h2 className="prefix">Email</h2>
+                    <p className="info">{email || "Unspecified"}</p>
+                </span>
+            </div>
+        </Card>
+    )
+}
+
+export default ClientCard
